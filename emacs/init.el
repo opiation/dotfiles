51dcc950cecd27f2e9a90afb7b75656372bd02cf
Nicholas Gelinas <ngelinas@pyx4.com>
2020-09-21 15:09:34 -0400

    Adds a number of configuration options
    * `projectile` for project management
    * `magit` for git integration
    * `solarized` for solarized themes

--- emacs/init.el
+++ emacs/init.el
@@ -48,9 +48,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("5f824cddac6d892099a91c3f612fcf1b09bb6c322923d779216ab2094375c5ee" default))
+   '("51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "13a8eaddb003fd0d561096e11e1a91b029d3c9d64554f8e897b2513dbf14b277" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "5f824cddac6d892099a91c3f612fcf1b09bb6c322923d779216ab2094375c5ee" default))
+ '(frame-brackground-mode 'dark)
  '(package-selected-packages
-   '(js2-mode editorconfig company tide neotree markdown-preview-mode visual-fill-column markdown-mode gruber-darker-theme smex typescript-mode)))
+   '(projectile elixir-mode magit solarized-theme js2-mode editorconfig company tide neotree markdown-preview-mode visual-fill-column markdown-mode gruber-darker-theme smex typescript-mode)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -65,7 +66,7 @@
 ;; Theme and display configuration
 ;;
 ;; Load default theme
-(load-theme 'gruber-darker)
+(load-theme 'solarized-gruvbox-dark)
 
 ;; Show line numbers in the left-hand side gutter
 (require 'display-line-numbers)
@@ -122,6 +123,24 @@
 ;; Editor config
 (editorconfig-mode 1)
 
+
+
+;; Projectile
+(unless (package-installed-p 'projectile)
+  (package-install 'projectile))
+(require 'projectile)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
+
+
+
+;; Magit setup
+(unless (package-installed-p 'magit)
+  (package-install 'magit))
+(require 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
+  
 
 
 ;; Language settings
