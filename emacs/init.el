f62d2bdb8b775edc28c4bffab89df50f330506dd
Nicholas Gelinas <ngelinas@pyx4.com>
2021-02-02 09:51:41 -0500

    Revamps emacs config significantly

--- emacs/init.el
+++ emacs/init.el
@@ -4,32 +4,164 @@
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+;; This will set the TERM environment variable to "tramp" when accessing a
+;; remote machine
+;; It is useful in particular to so that tramp doesn't have to parse
+;; elaborate shell prompts to understand when it is in a shell.  This
+;; expects shell configs on the remote target (`~/.bashrc`, `~/.zshrc`) to
+;; return early if detecting this env variable.
+(setq tramp-terminal-type "tramp")
 
+;; Be sure to install use-package with:
+;; package-install use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; Run this once to install required fonts
+;; all-the-icons-install-fonts
+(use-package all-the-icons
+  ;:config
+  ;(all-the-icons-install-fonts t)
+  :ensure t)
+
+;; Show line numbers in lefthandside gutter
+(use-package display-line-numbers
+  :hook (prog-mode . display-line-numbers-mode)
+  :ensure t)
+
+(use-package markdown-mode
+  :config (setq-default markdown-fontify-code-blocks-natively 1)
+  :ensure t)
+
+(use-package projectile
+  :config
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  :ensure t)
+
+(use-package treemacs
+  :bind (:map global-map
+	      ("<f8>" . treemacs))
+  :defer t
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after projectile treemacs 
+  :ensure t)
+
+(use-package treemacs-magit
+  :after magit treemacs
+  :ensure t)
+
+;; Magit Git plugin
+(use-package magit
+  :bind (:map global-map
+	      ("C-x g" . magit-status))
+  :ensure t)
+
+(use-package drag-stuff
+  :config (drag-stuff-define-keys)
+  :ensure t
+  :hook (prog-mode . drag-stuff-mode))
+
+;;
+;; Themes!!
+;;
+(use-package atom-one-dark-theme
+  :config (load-theme 'atom-one-dark t)
+  :ensure t)
+(use-package solarized-theme
+  :ensure t)
+(use-package zenburn-theme
+  :ensure t)
+
+(use-package centaur-tabs
+  :after all-the-icons
+  :bind
+  (:map global-map
+	("C-<prior>" . centaur-tabs-backward)
+	("C-<next>" . centaur-tabs-forward))
+  :demand
+  :config
+  (setq centaur-tabs-height 44
+;	centaur-tabs-modified-marker "*"
+	centaur-tabs-set-icons t
+	centaur-tabs-set-modified-marker t
+	centaur-tabs-show-navigation-buttons t
+	centaur-tabs-style "bar")
+  (centaur-tabs-mode 1)
+  :ensure t)
+
+(use-package powerline
+  :config (powerline-center-theme)
+  :ensure t)
+
+(use-package lsp-mode
+  :config
+  ;; (lsp-install-server ts-ls)
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (javascript-mode . lsp))
+         ;; if you want which-key integration
+         ;;(lsp-mode . lsp-enable-wqhich-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui
+  :after lsp-mode
+  :commands lsp-ui-mode
+  :ensure t)
+
+;; if you are helm user
+;;(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+;;(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+
+(use-package lsp-treemacs
+  :after lsp-mode treemacs
+  :commands lsp-treemacs-errors-list
+  :ensure t)
+
+(use-package editorconfig
+  :config (editorconfig-mode 1)
+  :ensure t)
+
+(use-package visual-fill-column
+  :config (setq-default fill-column 80)
+  :ensure t
+  :hook
+  (prog-mode . display-fill-column-indicator-mode))
 
 ;;
 ;; Using emacs in windowed mode
 ;;
 ;; Kill splash screen at startup
 (setq inhibit-startup-screen t)
+
 ;;
 ;; Remove menu and tool bars from the window
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 
+;;
+;; Set an appropriate font and size depending on the machine
+;;
+(cond
+ ((equal (system-name) "betsy")
+  (set-face-attribute 'default nil :font "Fira Code-12"))
+ ((equal (system-name) "dellbot")
+  (set-face-attribute 'default nil :font "Fira Code-14")))
 
-
-;; `smex` configuration
-;;
-;; Install using the following:
-;;
-;; package-install [RET] smex
-;;
 ;; Use `smex` package for auto-completion in Emacs commands
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-
-
+(use-package smex
+  :bind (
+	 ("M-x" . smex)
+	 ("M-X" . smex-major-mode-commands)
+	 ("C-c C-c M-x" . execute-extended-command))
+  :ensure t)
 
 ;; Auto-complete in some commands
 (ido-mode 1)
@@ -41,52 +173,23 @@
 
 
 
-;; Generated configuration
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "13a8eaddb003fd0d561096e11e1a91b029d3c9d64554f8e897b2513dbf14b277" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "5f824cddac6d892099a91c3f612fcf1b09bb6c322923d779216ab2094375c5ee" default))
- '(frame-brackground-mode 'dark)
- '(package-selected-packages
-   '(projectile elixir-mode magit solarized-theme js2-mode editorconfig company tide neotree markdown-preview-mode visual-fill-column markdown-mode gruber-darker-theme smex typescript-mode)))
 
-(custom-set-faces
+
+;; Generated configuration
+;;(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-
-
-
-;;
-;; Theme and display configuration
-;;
-;; Load default theme
-(load-theme 'solarized-gruvbox-dark)
-
-;; Show line numbers in the left-hand side gutter
-(require 'display-line-numbers)
-(global-display-line-numbers-mode)
+;; '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 143 :width normal)))))
 
 ;; Word wrap
 ;;
 ;; This ensure lines are wrapped at word boundaries instead of inbetween words
 ;; at exactly the edge
 ;;
-;; (require 'visual-line-mode)
 (global-visual-line-mode 1)
 
-;; (require 'visual-fill-column)
-(global-visual-fill-column-mode 1)
-(setq-default fill-column 80)
-(global-display-fill-column-indicator-mode 1)
-
-(require 'markdown-mode)
-(setq-default markdown-fontify-code-blocks-natively 1)
 
 ;;
 ;; Session tabs
@@ -94,13 +197,9 @@
 ;; Frame-based tab bar and related shortcuts
 ;; Use [SUPER] and left/right arrows to navigate between tabs
 ;;
-(tab-bar-mode 1)
-(global-set-key [M-left] 'tab-bar-switch-to-prev-tab)
-(global-set-key [M-right] 'tab-bar-switch-to-next-tab)
-
-;; Directory tree viewing with neotree
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
+;;(tab-bar-mode 1)
+;;(global-set-key [M-left] 'tab-bar-switch-to-prev-tab)
+;;(global-set-key [M-right] 'tab-bar-switch-to-next-tab)
 
 ;; Typescript support
 (defun setup-tide-mode ()
@@ -119,30 +218,15 @@
 (setq tide-format-options '(:indentSize 2
 			    :tabSize 2))
 
-
-;; Editor config
-(editorconfig-mode 1)
-
-
-
-;; Projectile
-(unless (package-installed-p 'projectile)
-  (package-install 'projectile))
-(require 'projectile)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-(projectile-mode +1)
-
-
-
-;; Magit setup
-(unless (package-installed-p 'magit)
-  (package-install 'magit))
-(require 'magit)
-(global-set-key (kbd "C-x g") 'magit-status)
-  
-
-
 ;; Language settings
 (setq js-indent-level 2
       typescript-indent-level 2)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(message "Hey, there!  We're done loading!")
